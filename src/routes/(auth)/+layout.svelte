684085447e7cdd5e8889d2d995717f6d0901b7db
John Carlo Asilo <jcarloasilo@gmail.com>
2024-05-07 14:50:50 +0800

    add auth utils

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,8 @@
+<div class="flex min-h-screen flex-col">
+	<header class="h-16 border-b">
+		<div class="container flex h-full items-center">
+			<div class="text-2xl font-bold">POPMS</div>
+		</div>
+	</header>
+	<slot />
+</div>
---
+++ src/routes/(auth)/reset/+page.svelte
@@ -0,0 +1,3 @@
+<main class="container flex max-w-md flex-1 flex-col items-center gap-y-6 py-24">
+	<div>WORK HEREs</div>
+</main>
