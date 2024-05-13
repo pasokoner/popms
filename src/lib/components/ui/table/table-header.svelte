330323e2a61db9baf06139b47b119da03d779647
John Carlo Asilo <jcarloasilo@gmail.com>
2024-05-13 14:11:57 +0800

    add shadcn table & sheet

---
+++ src/lib/components/ui/table/table-header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { HTMLAttributes } from "svelte/elements";
+	import { cn } from "$lib/utils.js";
+
+	type $$Props = HTMLAttributes<HTMLTableSectionElement>;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<thead
+	class={cn("bg-gray-100 text-gray-400 [&_tr]:border-b", className)}
+	{...$$restProps}
+	on:click
+	on:keydown
+>
+	<slot />
+</thead>
