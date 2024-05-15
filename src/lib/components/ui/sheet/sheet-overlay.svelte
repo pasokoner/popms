4f4c8804fed089eabb72464dfef98cd4672245d7
John Carlo Asilo <jcarloasilo@gmail.com>
2024-05-15 15:51:52 +0800

    remove blur and adjust overlay opacity

--- src/lib/components/ui/sheet/sheet-overlay.svelte
+++ src/lib/components/ui/sheet/sheet-overlay.svelte
@@ -16,6 +16,6 @@
 <SheetPrimitive.Overlay
 	{transition}
 	{transitionConfig}
-	class={cn("fixed inset-0 z-50 bg-black/80 backdrop-blur-sm ", className)}
+	class={cn("fixed inset-0 z-50 bg-black/50 ", className)}
 	{...$$restProps}
 />
