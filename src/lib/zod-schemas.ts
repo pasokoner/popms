9e3ad9108d0ce99eea2d74724e0b10b05baf44c3
John Carlo Asilo <jcarloasilo@gmail.com>
2024-05-16 14:42:27 +0800

    add schema for creating dept

--- src/lib/zod-schemas.ts
+++ src/lib/zod-schemas.ts
@@ -5,4 +5,11 @@ export const loginSchema = z.object({
 	password: z.string().min(1, { message: "Type your password here" })
 });
 
+export const createDepartmentSchema = z.object({
+	name: z.string().min(1, { message: "Type your department name here" }),
+	email: z.string().email("Type your email here"),
+	password: z.string().min(8, { message: "Minimum 8 characters" })
+});
+
 export type LoginSchema = typeof loginSchema;
+export type CreateDepartmentSchema = typeof createDepartmentSchema;
