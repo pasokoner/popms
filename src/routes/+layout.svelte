9e81f7915aee008d9344071018a9e18c1e707912
John Carlo Asilo <jcarloasilo@gmail.com>
2024-05-27 16:43:12 +0800

    add toaster

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
 	import "../app.pcss";
+	import { Toaster } from "$lib/components/ui/sonner";
 </script>
 
 <svelte:head>
 	<title>POPMS</title>
 </svelte:head>
+
+<Toaster richColors theme="light" />
 
 <slot />
